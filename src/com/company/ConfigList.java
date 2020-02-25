a4e0476fe58c7c94576c703b273d12aa01843453
Paul <Paul@mail.com>
2020-02-25 10:48:21 +0300

    Added class ConfigList.

---
+++ src/com/company/ConfigList.java
@@ -0,0 +1,24 @@
+package com.company;
+
+class ConfigList{
+    private String userName;
+    private String serverURL;
+    private String outDirName;
+    private String linksFileName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
+    public void setOutDirName(String outDirName) { this.outDirName = outDirName; }
+    public void setLinksFileName(String linksFileName) {
+        this.linksFileName = linksFileName;
+    }
+
+    public String getUserName() { return userName; }
+    public String getServerURL() { return serverURL; }
+    public String getOutDirName() { return outDirName; }
+    public String getLinksFileName() { return linksFileName; }
+}
